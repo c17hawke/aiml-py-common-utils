c06009ce76aad0086ab71c95e579ed8a8c482df3
c17hawke <sunny.c17hawke@gmail.com>
2024-03-05 16:13:22 +0530

    updated save as json from dict2json

--- tests/unit/test_common_utils.py
+++ tests/unit/test_common_utils.py
@@ -1,9 +1,9 @@
-from aiml_py_common_utils import save_dict2json
+from aiml_py_common_utils import save_as_json
 import json
 import pytest # noqa
 from pathlib import Path
 
-def test_save_dict2json(mocker):
+def test_save_as_json(mocker):
     # Mocking the open function, json.dump and logger.debug
     mocker.patch('builtins.open', mocker.mock_open())
     mocker.patch('json.dump')
@@ -11,7 +11,7 @@ def test_save_dict2json(mocker):
     test_path = Path('test.json')
     test_data = {"key": "value"}
 
-    save_dict2json(test_path, test_data)
+    save_as_json(test_path, test_data)
 
     # Assert that json.dump was called with the correct arguments
     json.dump.assert_called_once_with(test_data, mocker.ANY, indent=4)
