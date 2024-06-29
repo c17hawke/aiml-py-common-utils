4a1284754379e003d2c4e2fe3381460a9dd053e6
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2024-06-29 17:29:12 +0000

    token count for gpt4 and all openai embedding models added

--- src/aiml_py_common_utils/gen_ai/__init__.py
+++ src/aiml_py_common_utils/gen_ai/__init__.py
@@ -1,5 +1,7 @@
-from aiml_py_common_utils.gen_ai.llm_openai import num_of_tokens_gpt35_turbo
+from aiml_py_common_utils.gen_ai.llm_openai import num_of_tokens_gpt35_turbo, num_of_tokens_gpt4, num_of_tokens_embedding_models
 
 __all__ = [
-    'num_of_tokens_gpt35_turbo'
+    'num_of_tokens_gpt35_turbo',
+    'num_of_tokens_gpt4',
+    'num_of_tokens_embedding_models'
 ]
--- src/aiml_py_common_utils/gen_ai/llm_openai.py
+++ src/aiml_py_common_utils/gen_ai/llm_openai.py
@@ -1,8 +1,22 @@
+"""
+Reference - https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+--------------------------------------------------------------------------
+Encoding name          | OpenAI models
+-----------------------|--------------------------------------------------
+cl100k_base            | gpt-4, gpt-3.5-turbo, text-embedding-ada-002, 
+                       | text-embedding-3-small, text-embedding-3-large
+p50k_base              | Codex models, text-davinci-002, text-davinci-003
+r50k_base (or gpt2)    | GPT-3 models like davinci
+"""
+
 import tiktoken
 from tiktoken import Encoding
 
 # Load the GPT-3.5-turbo model
 enc_gpt35_turbo = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+# encoding for gpt-4, gpt-3.5-turbo, text-embedding-ada-002, text-embedding-3-small, text-embedding-3-large
+encoding = tiktoken.get_encoding("cl100k_base")
 
 def _num_of_tokens(string: str, enc: Encoding) -> int:
     """calculate number of tokens from input string as per the LLM model encoding
@@ -23,6 +37,33 @@ def num_of_tokens_gpt35_turbo(string: str, enc: Encoding=enc_gpt35_turbo) -> int
     Args:
         string (str): string input for calculating number of tokens
         enc (Encoding): encoding to used for encoding strings as per the GPT3.5-Turbo model
+
+    Returns:
+        int: number of tokens in string passed as argument
+    """
+    return _num_of_tokens(string=string, enc=enc)
+
+def num_of_tokens_gpt4(string: str, enc: Encoding=encoding) -> int:
+    """calculate number of tokens from input string as per the gpt-4 model encoding
+
+    Args:
+        string (str): string input for calculating number of tokens
+        enc (Encoding): encoding to used for encoding strings as per the gpt-4 model
+
+    Returns:
+        int: number of tokens in string passed as argument
+    """
+    return _num_of_tokens(string=string, enc=enc)
+
+def num_of_tokens_embedding_models(string: str, enc: Encoding=encoding) -> int:
+    """Calculate the number of tokens in the input string for the 
+        text-embedding-ada-002, 
+        text-embedding-3-small, and 
+        text-embedding-3-large embedding models.
+
+    Args:
+        string (str): string input for calculating number of tokens
+        enc (Encoding):The encoding to be used as per the 3 models
 
     Returns:
         int: number of tokens in string passed as argument
