acd8d7b27a6756d5f7b2ad2af141a1fd1d9aad29
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2024-06-29 19:10:36 +0000

    updates test for gpt4 and embeding model token len calc

--- tests/unit/test_genai_openai_utils.py
+++ tests/unit/test_genai_openai_utils.py
@@ -3,8 +3,8 @@ from tiktoken import Encoding
 from _pytest.fixtures import FixtureFunction
 
 # Assuming your functions are in a module named 'my_module'
-from aiml_py_common_utils.gen_ai import num_of_tokens_gpt35_turbo
-from aiml_py_common_utils.gen_ai.llm_openai import enc_gpt35_turbo
+from aiml_py_common_utils.gen_ai import num_of_tokens_gpt35_turbo, num_of_tokens_gpt4, num_of_tokens_embedding_models
+from aiml_py_common_utils.gen_ai.llm_openai import enc_gpt35_turbo, encoding
 
 def test_num_of_tokens_gpt35_turbo_mocker(mocker: FixtureFunction) -> None:
     # Mock the Encoding object and its encode method
@@ -28,6 +28,26 @@ def test_num_of_tokens_gpt35_turbo() -> None:
 
     # Call the function with the mocked encoding
     result = num_of_tokens_gpt35_turbo(test_string, enc=enc_gpt35_turbo)
+
+    # Assert that the function returned the correct result
+    assert result == 6  # This should match the length of the list returned by the mocked encode method
+
+def test_num_of_tokens_gpt4() -> None:
+    # Mock the Encoding object and its encode method
+    test_string = "This is a test string."
+
+    # Call the function with the mocked encoding
+    result = num_of_tokens_gpt4(test_string, enc=encoding)
+
+    # Assert that the function returned the correct result
+    assert result == 6  # This should match the length of the list returned by the mocked encode method
+
+def test_num_of_tokens_embedding_models() -> None:
+    # Mock the Encoding object and its encode method
+    test_string = "This is a test string."
+
+    # Call the function with the mocked encoding
+    result = num_of_tokens_embedding_models(test_string, enc=encoding)
 
     # Assert that the function returned the correct result
     assert result == 6  # This should match the length of the list returned by the mocked encode method
